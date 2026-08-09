ad4154e414004d6205b1d1bace6e3614622792b3
20230104Curl <sanaru0104@gmail.com>
2026-08-09 18:32:17 +0900

    Update apiUrl in config.js to production URL

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 window.BUNMYAKU_CONFIG = Object.freeze({
   // Google Apps Script のウェブアプリURLを設定します。
-  apiUrl: "__APPS_SCRIPT_WEB_APP_URL__"
+  apiUrl: "https://script.google.com/macros/s/AKfycbwmPDttJOj4ckqgDJWbNAzRLMLt2-tOtD8Vsxd_0THefqPFq3guyupYWq9h1G333S5R/exec"
 });
